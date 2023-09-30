70246171f828306bcc2526f67fde8d92fe81d9d7
Shekhar Biswas <32758439+shekharbiswas@users.noreply.github.com>
2023-09-30 23:20:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 
 
 import time  # to simulate a real time data, time loop
-
+import yfinance as yf
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+import yahoo_fin.stock_info as si
 import numpy as np  # np mean, np random
 import pandas as pd  # read csv, df manipulation
 import plotly.express as px  # interactive charts
@@ -13,82 +16,75 @@ st.set_page_config(
     layout="wide",
 )
 
-# read csv from a github repo
-dataset_url = "https://raw.githubusercontent.com/Lexie88rus/bank-marketing-analysis/master/bank.csv"
 
-# read csv from a URL
-@st.experimental_memo
-def get_data() -> pd.DataFrame:
-    return pd.read_csv(dataset_url)
+info = pd.read_csv('nasdaq.csv')
 
-df = get_data()
+
 
 # dashboard title
 st.title("Real-Time Stock Price")
 
 # top-level filters
-job_filter = st.selectbox("Select the Job", pd.unique(df["job"]))
+code = st.selectbox("Select the Job", pd.unique(info["Symbol"]))
 
 # creating a single-element container
 placeholder = st.empty()
 
 # dataframe filter
-df = df[df["job"] == job_filter]
+data = yf.download(code,'2023-01-01','2023-12-31')
+data = data.reset_index()
+df = data
+
 
 # near real-time / live feed simulation
 for seconds in range(200):
 
-    df["age_new"] = df["age"] * np.random.choice(range(1, 5))
-    df["balance_new"] = df["balance"] * np.random.choice(range(1, 5))
+    df['5D'] = df['Adj Close'].shift(5)
+    df['10D'] = df['Adj Close'].shift(10)    # chnage 10, 20 days
+    df['change'] = round((df['Adj Close'].diff() / df['Adj Close'].abs().shift(1))*100,2)
+    df['price_vol'] = (df['Adj Close']/ df['Volume'])*10**7*2
+    df = df.drop(columns = ['Open', 'High', 'Low', 'Close'])
+    df['w_5'] = round((df['Adj Close']/ df['5D'] -1)*100,2)
+    df['w_10'] = round((df['Adj Close']/ df['10D'] -1)*100,2)
+    df['p_10'] = df['Adj Close'] - df['10D']
+    df['p_10'] = df['p_10'].apply(lambda x: round(x,2 ))
 
-    # creating KPIs
-    avg_age = np.mean(df["age_new"])
 
-    count_married = int(
-        df[(df["marital"] == "married")]["marital"].count()
-        + np.random.choice(range(1, 30))
-    )
+    df['v5D'] = df['Volume'].shift(5)
+    df['v10D'] = df['Volume'].shift(10) # 10 , 20 days
+    df['v_5'] = round((df['Volume']/ df['v5D'] -1)*100,6)
+    # momentum 10 day purpose
+    df['v_10'] = round((df['Volume']/ df['v10D'] -1)*100,6)
+    buy_ratings = list(range(0,-10,-1)) # rolling 10 and this is 2x
 
-    balance = np.mean(df["balance_new"])
 
-    with placeholder.container():
 
-        # create three columns
-        kpi1, kpi2, kpi3 = st.columns(3)
+    df['change_pct_1'] = round((df['w_5'].diff() / df['w_5'].abs().shift())*100,2)
+    df['vol_pct_1'] = round((df['Volume'].diff() / df['Volume'].abs().shift())*100,2)
+    df['vol_pct_2'] = round((df['vol_pct_1'].diff() / df['vol_pct_1'].abs().shift())*100,2)
+    df['vol_pct_2_'] = df['vol_pct_2'].diff().fillna(df['Volume'])
+    asign = np.sign(df['vol_pct_2_'] )
+    signchange = ((np.roll(asign, 1) - asign) != 0).astype(int)
+    df['sign_change'] = signchange
 
-        # fill in those three columns with respective metrics or KPIs
-        kpi1.metric(
-            label="Age ⏳",
-            value=round(avg_age),
-            delta=round(avg_age) - 10,
-        )
-        
-        kpi2.metric(
-            label="Married Count 💍",
-            value=int(count_married),
-            delta=-10 + count_married,
-        )
-        
-        kpi3.metric(
-            label="A/C Balance ＄",
-            value=f"$ {round(balance,2)} ",
-            delta=-round(balance / count_married) * 100,
-        )
 
-        # create two columns for charts
-        fig_col1, fig_col2 = st.columns(2)
-        with fig_col1:
-            st.markdown("### First Chart")
-            fig = px.density_heatmap(
-                data_frame=df, y="age_new", x="marital"
-            )
-            st.write(fig)
-            
-        with fig_col2:
-            st.markdown("### Second Chart")
-            fig2 = px.histogram(data_frame=df, x="age_new")
-            st.write(fig2)
 
-        st.markdown("### Detailed Data View")
-        st.dataframe(df)
-        time.sleep(1)
+    df = df[['Date','Adj Close', 'Volume', '5D', 'w_5', 'v5D', 'v_5', 'v_10', 'p_10', 'w_10']]
+
+    try:
+        df = df.drop(df[df.v_10 == np.inf].index[0]).reset_index(drop = True)
+    except:
+        pass
+
+    figure(figsize=(18, 6), dpi=80)
+    v_10_range = df['v_10'].max() - df['v_10'].min()
+    p_10_range = df['p_10'].max() - df['p_10'].min()
+    df['v_10'] = df['v_10']/ v_10_range
+    df['p_10'] = df['p_10']/ p_10_range
+    df.index = df.Date
+    df['p_10'].plot()
+    df['v_10'].plot()
+    plt.axhline(y=0, color='r', linestyle='-')
+    plt.title(code)
+
+    time.sleep(3)
