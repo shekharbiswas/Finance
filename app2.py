160b2a3aa9518e91c3843178f285c1313112a3db
shekharbiswas <biswas.berlin89@gmail.com>
2024-02-11 21:23:37 +0100

    ss

--- app2.py
+++ app2.py
@@ -57,7 +57,7 @@ with col2:
 
     st.write(s_date , e_date)
 
-    if (d[1] - d[0]) >= datetime.timedelta(days=7) :
+    if (d[1] - d[0]) >= datetime.timedelta(days=6) :
 
         st.write('7 days')
 
