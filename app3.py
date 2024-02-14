ebfa1133f0b634b68624dd2d832ad8552a53908a
shekharbiswas <shekhar07.nitdgp@gmail.com>
2024-02-14 10:52:04 +0100

    ss

--- app3.py
+++ app3.py
@@ -229,6 +229,8 @@ with col2:
 
                 df1 = df1.dropna()
 
+                l1 = []
+
                 l1 = list(df1.resample('1w')['VC'].mean().tail(5))
                 st.write(l1)
                 recom = round(sum([i for i in l1 if i >= 0 ]) / sum([abs(number) for number in l1]), 2)*100
